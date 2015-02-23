d69f908e975a7ce13022e320f5f46b92ad12e4ed
Jonathan Davis <cartman61616@gmail.com>
2015-02-22 20:06:05 -0500

    New game
    restarted development

---
+++ src/game/Main.java
@@ -0,0 +1,13 @@
+package game;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		Random rand = new Random();
+	}
+
+}
--- src/objects/LocationDemo.java
+++
@@ -1,9 +0,0 @@
-package objects;
-
-import java.applet.Applet;
-
-public class LocationDemo extends Applet {
-	Location currentLocation;
-	String command;
-	
-}
