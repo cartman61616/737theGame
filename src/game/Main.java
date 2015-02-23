dc007b5e836de88c78b943fbcc126a2bf5d0c0b3
Jonathan Davis <cartman61616@gmail.com>
2015-02-23 11:42:04 -0500

    Beginning the 737 journey
    Changed some variables, added variables and new choices.

--- src/game/Main.java
+++ src/game/Main.java
@@ -13,25 +13,59 @@ public class Main {
 
 		// Game variables
 		String[] enemies = { "Skeleton", "Zombie", "Warrior", "Assassin" };
+		String boss1 = "Ticketmaster";
+		String boss2 = "TSA";
+		String boss3 = "Buckminster Staff";
+		String finalBoss = "HoB Staff";
+		int boss1Health = 75;
+		int boss2Health = 85;
+		int boss3Health = 90;
+		int finalBossHealth = 100;
 		int maxEnemyHealth = 75;
-		int enemyAttackDamage = 25;
+		int enemyAttackDamage = 15;
 
 		// Player variables
 		int health = 100;
-		int attackDmg = 50;
-		int numHealthPots = 3;
-		int healthPotionHealAmount = 30;
-		int healthPotionDropChance = 50; // Percentage
+		int mana = 100;
+		int attackDmg = 20;
+		int numPizzaSlices = 3;
+		int numBeers = 3;
+		int beerManaAmount = 20;
+		int pizzaSliceHealAmount = 30;
+		int pizzaSliceDropChance = 30; // Percentage
+		int beerDropChance = 30;
 
 		boolean running = true;
 
-		System.out.println("Welcome to the dungeon!");
+		System.out.println("Welcome to 737 the Game!");
+		System.out.println("The journey to HomeTown ThrowDown!");
 
 		// Label
 		GAME:
 			while (running) {
 				System.out.println("--------------------------------------");
-
+				
+				System.out.println("Please choose your starting point");
+				System.out.println("1. Los Angeles, California");
+				System.out.println("2. Atlanta, Georgia");
+				System.out.println("3. New York, New York");
+				System.out.println("4. Minneapolis, Minnesota");
+				System.out.println("5. Brunswick, Maine");
+				String input = in.nextLine();
+				if (input.equals("1")) {
+					// need to implement
+				} else if (input.equals("2")) {
+					// need to implement
+				} else if (input.equals("3")) {
+					// need to implement
+				} else if (input.equals("4")) {
+					// need to implement
+				} else if (input.equals("5")) {
+					// need to implement
+				} else {
+					System.out.println("That isn't a valid city. Contact the lazy developer to add it");
+				}
+				
 				int enemyHealth = rand.nextInt(maxEnemyHealth);
 				String enemy = enemies[rand.nextInt(enemies.length)];
 				System.out.println("\t# " + enemy + " has appeared! #\n");
@@ -41,8 +75,10 @@ public class Main {
 					System.out.println("\t" + enemy + "'s HP: " + enemyHealth);
 					System.out.println("\n\tWhat would you like to do?");
 					System.out.println("\t1. Attack");
-					System.out.println("\t2. Drink health potion");
-					System.out.println("\t3. Run");
+					System.out.println("\t2. Use Beer Powers");
+					System.out.println("\t3. Eat Pizza");
+					System.out.println("\t4. Drink a Beer");
+					System.out.println("\t5. Run");
 
 					String input = in.nextLine();
 					if (input.equals("1")) {
@@ -60,18 +96,22 @@ public class Main {
 							break;
 						}
 					} else if (input.equals("2")) {
+						//need to implement
+					} else if (input.equals("3")) {
 
-						if (numHealthPots > 0) {
-							health += healthPotionHealAmount;
-							numHealthPots--;
-							System.out.println("\t> You drank a health potion, healed for: " + healthPotionHealAmount + "."
+						if (numPizzaSlices > 0) {
+							health += pizzaSliceHealAmount;
+							numPizzaSlices--;
+							System.out.println("\t> You ate a slice of pizza, healed for: " + pizzaSliceHealAmount + "."
 									+ "\n\t> You now have " + health + "HP."
-									+ "\n\t> You now have " + numHealthPots + " health potions left.\n");
+									+ "\n\t> You now have " + numPizzaSlices + " health potions left.\n");
 						} else {
-							System.out.println("\t> You have no health potions, defeat enemies for a chance to get one");
+							System.out.println("\t> You have no pizza left, defeat enemies for a chance to get one");
 						}
 
-					} else if (input.equals("3")) {
+					} else if (input.equals("6")) {
+						//need to implement
+					} else if (input.equals("5")) {
 						System.out.println("\t> You run away from the " + enemy);
 						continue GAME;
 					} else {
@@ -86,10 +126,10 @@ public class Main {
 				System.out.println(" # " + enemy + " was defeated! # ");
 				System.out.println(" # You have " + health + "HP left # ");
 				// If the random number is less than 50 it drops
-				if (rand.nextInt(100) < healthPotionDropChance) {
-					numHealthPots++;
+				if (rand.nextInt(100) < pizzaSliceDropChance) {
+					numPizzaSlices++;
 					System.out.println(" # The " + enemy + " dropped a health potion. # ");
-					System.out.println(" # You now have " + numHealthPots + " health potion(s). # ");
+					System.out.println(" # You now have " + numPizzaSlices + " health potion(s). # ");
 				}
 				System.out.println("--------------------------------------");
 				System.out.println("What would you like to do now?");
